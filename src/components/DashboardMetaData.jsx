cd9050a224aac83342d2532746022bcd6ff40a4f
uos-datavisdashboard <uchekesla@gmail.com>
2022-10-18 23:57:43 +0100

    make it better

--- src/components/DashboardMetaData.jsx
+++ src/components/DashboardMetaData.jsx
@@ -1,6 +1,4 @@
-///////////////////////////////////
-// INTERFACE FOR THE PLOTLY LIBRARY
-///////////////////////////////////
+// you can test those functions https://codepen.io/pen
 
 /**
  * This is an interface for the Plotly Library
@@ -14,46 +12,14 @@
  * - gridColor - this changes the color of the grid within the plot
  */
 export default class PlotlyInterface {
-  /*
-
-  - Take the plotly object from the window
-  - to allow this make sure that the plotly cdn is included in the index.html document
-  - ensure that the plotly constant is initialized within a useEffect or a componentDidMount callback
-  - this will ensure that the div with the plotly ID is in the DOM when the function runs 
-
-  */
-  constructor(canvasID, plotly) {
+  constructor(canvasID) {
     this.canvasID = canvasID;
-    this.plotly = plotly;
-    this.plotTitle = "Generated Number Stream";
-    this.yTicks = "Value";
-    this.xTicks = "Epoch Time";
-    this.fontColor = "#76889a";
-    this.plotColor = "#1b2444";
-    this.gridColor = "#525f89";
-    this.layout = {
-      title: this.plotTitle,
-      font: { color: this.fontColor },
-      paper_bgcolor: this.plotColor,
-      plot_bgcolor: this.plotColor,
-      yaxis: {
-        title: this.yTicks,
-        gridcolor: this.gridColor,
-        titlefont: {
-          family: "Arial, sans-serif",
-          size: 18,
-          color: this.fontColor,
-        },
-      },
-      xaxis: {
-        title: this.xTicks,
-        titlefont: {
-          family: "Arial, sans-serif",
-          size: 18,
-          color: this.fontColor,
-        },
-      }
-    };
+    this.plotly = window.Plotly;
+
+    // this is a collection of traces
+    this.plotData = [];
+    this.layout = new PlotLayout("scatter");
+
     this.config = {
       responsive: true,
       editable: true,
@@ -61,9 +27,43 @@ export default class PlotlyInterface {
       displayModeBar: true,
       scrollZoom: false,
       showLink: false,
+      linkText: "This text is custom!",
       plotlyServerURL: "https://chart-studio.plotly.com",
     };
   }
+
+  addTrace = (type, name) => {
+    let orientation = "h";
+    let trace = new Trace(type, name);
+    if (type === "box") {
+      if (orientation === "h") {
+        // orientation h -> x otherwise y
+      }
+      if (seeUnderlyingData) {
+        trace.addUnderlyingData();
+      }
+    }
+    this.plotData.push(trace);
+    return this;
+  };
+
+  addPlotTitle = (title) => {
+    this.layout.title = title;
+    return this;
+  };
+
+  addDimension = (axis, data, label, traceID) => {
+    const trace = this.plotData[traceID];
+    if (axis === "x") {
+      this.trace.x = data;
+      this.layout.xaxis.title = label;
+    }
+  };
+
+  // similar to contours and heatmaps, surfaces accept an array of arrays as a z axis
+  // scatter, scatter3d, pie, bar, box, histogram, histogram2dcontour, contour, surface
+  // mode includes : markers lines, lines+scatter
+  changePlotType = () => {};
 
   /**
    * Build a plot from scratch
@@ -72,8 +72,10 @@ export default class PlotlyInterface {
    *  - i.e. plotData = [ trace1, trace2, ....]
    *  - where traces are { x: [], y: [], mode: "markers", markers : {color : 'blue'}}
    */
-  constructInitialPlot(plotData) {
-    this.plotly.newPlot(this.canvasID, plotData, this.layout, this.config);
+  constructInitialPlot(plotData, layout) {
+    plotData = plotData === undefined ? this.plotData : plotData;
+    layout = layout === undefined ? this.layout : layout;
+    this.plotly.newPlot(this.canvasID, plotData, layout, this.config);
   }
 
   /**
@@ -83,15 +85,6 @@ export default class PlotlyInterface {
    * @param newDataX - this is an array of arrays containing the last x value of each trace
    */
   updateInitialPlot(newDataY, newDataX) {
-    /*
-
-    - new Data is an object with a y and an x property which are array of arrays
-
-    - [[trace1],[trace2],[trace3]...[traceN]]
-    - where N is the size of the outer array
-    - trace ids are required by the extendTraces method
-    
-    */
     const traceIDs = [];
     for (let i = 0; i < newDataY.length; i++) {
       traceIDs.push(i);
@@ -105,12 +98,7 @@ export default class PlotlyInterface {
       },
       traceIDs
     );
-    /*
-    
-    - to ensure that the graph always displays the maximum and the minimum values
-    - update the layout as the traces are added
 
-    */
     let dataMatrix = [];
     newDataY.forEach((val) => {
       dataMatrix.push(val[0]);
@@ -125,70 +113,244 @@ export default class PlotlyInterface {
     }
   }
 
-  /**
-   *
-   * @param {*} x - used as x values fro the scatter plots and bar charts,
-   * as labels for the pie chart and ignored for heat maps
-   * @param {*} xTicks - used for the x labels
-   * @param {*} y - used as y values for the scatter plots an the bar charts,
-   * used as values for the pie chart and z for the heat map
-   * @param {*} yTicks - used for the y labels
-   * @param {*} type - the plots can be either [ scatter, scatter3d, bar, heatmap and pie ]
-   * @param {*} mode - the mode of the graph is either [ lines, lines+markers, markers ]
-   * @param {*} markerSymbol - this is the symbol of the marker ["circle", "square", "diamond", "cross"]
-   * @param {*} date - this is a boolean which makes the x values date times if true
-   * @returns trace - object of the following form { x, y, type, mode, marker, opacity, line }
-   */
-  createPlotData(x, y, type, mode, markerSymbol, date) {
-    this.layout.xaxis.type = date ? "date" : "";
+  // unlike the update initial plot function the update plot will update the plot in place
+  // this is suitable for none real time data updates
+  updatePlot() {}
+}
 
-    if (type === "bar") return { x, y, type, mode };
-    if (type === "heatmap") return { z: y, type };
+// use the state design pattern to make the trace component
 
-    let opacity = 0.7;
-    let marker = {
-      size: 10,
-      opacity: 0.5,
-      symbol: markerSymbol,
-      line: { width: 1, dash: "dot" },
-    };
-    let line = mode.includes("lines") ? { width: 2 } : {};
-
-    let trace =
-      ["scatter3d", "scatter"].indexOf(type) !== -1
-        ? { x, y, type, mode, marker, opacity, line }
-        : { labels: x, values: y, type: "pie" };
-    if (type === "scatter3d") {
-      trace = { ...trace, z: trace.y };
-      this.config.scrollZoom = true;
-    }
-
-    return trace;
+export class Trace {
+  constructor(type, name) {
+    this.name = name;
+    this.type = type;
+    this.mode = "markers";
+    this.x = [];
+    this.y = [];
+    this.z = [];
+    this.showscale = true;
   }
 
-  /**
-   * This function allows to change the layout of the instanced plot
-   * after running this function call the construct initial plot function
-   * @param {*} theme - can be either [seaborn, dark, light, gray ]
-   * @param {*} plotTitle - string if left "" default title will be used
-   * @param {*} yTicks - string if left "" default title will be used
-   * @param {*} xTicks - string if left "" default title will be used
-   */
-  changeLayout(theme, plotTitle, yTicks, xTicks) {
-    let themes = {
-      seaborn: "rgb(234,234,242)",
-      dark: "black",
-      light: "white",
-      gray: "rgb(229,236,246)",
-    };
-    console.log(theme);
+  // a line plot comprise a mode of lines and a type of scatter, this can be edited from the line object
 
-    this.layout.paper_bgcolor = themes[`${theme}`];
-    this.layout.plot_bgcolor = themes[`${theme}`];
-    this.layout.yaxis.gridColor =
-      theme === "dark" || theme === "light" ? "rgb(50,50,20)" : "white";
-    this.xTicks = xTicks === "" ? this.xTicks : xTicks;
-    this.yTicks = yTicks === "" ? this.yTicks : yTicks;
-    this.plotTitle = plotTitle === "" ? this.plotTitle : plotTitle;
+  addDottedLine = (lineWidth) => {
+    this.line = {
+      dash: "dashdot",
+      width: lineWidth,
+    };
+  };
+
+  addMarker = (color, opacity, size, lineColor, lineWidth) => {
+    this.marker = {
+      color: color, // to make color change make it an array
+      opacity: opacity,
+      size: size, // can also be an array []
+      line: {
+        color: lineColor,
+        width: lineWidth,
+      },
+      symbol: "circle", // this can be other things but please don't
+    };
+    return this;
+  };
+
+  // size is an array
+  addRelativeSizeToMarkers = (size) => {
+    const desired_maximum_marker_size = 40;
+    this.marker = {
+      ...this.marker,
+      //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
+      size: size,
+      sizeref: (2.0 * Math.max(...size)) / desired_maximum_marker_size ** 2,
+      sizemode: "area",
+    };
+    return this;
+  };
+
+  // these can be a special type of scatter plots
+  addErrorBars = (direction, error_data) => {
+    let data = {
+      type: "data", // percent
+      visible: true,
+      type: "constant",
+      value: error_data, // this is a number be up to 10
+      color: "#85144B",
+      thickness: 1.5,
+      width: 3,
+      opacity: 1,
+    };
+    if (direction === "y") {
+      this.error_y = data;
+    } else {
+      this.error_x = data;
+    }
+
+    return this;
+  };
+
+  // pie chart methods
+
+  addValues = (values) => {
+    this.values = values;
+    return this;
+  };
+
+  addLabels = (labels) => {
+    this.labels = labels;
+    return this;
+  };
+
+  addDomain = (row, column) => {
+    this.domain = {
+      row: row,
+      column: column,
+    };
+    return this;
+  };
+
+  // box plot method
+
+  addOutlierDetection = () => {
+    // implement boxpoints of suspectedoutliers to display the outlier
+    this.marker = {
+      ...this.marker,
+      color: "rgb(8,81,156)",
+      outliercolor: "rgba(219, 64, 82, 0.6)",
+      line: {
+        outliercolor: "rgba(219, 64, 82, 1.0)",
+        outlierwidth: 2,
+      },
+    };
+    return this;
+  };
+
+  addUnderlyingData = () => {
+    this.boxpoints = "all"; // can also be false to only show the whiskers and suspectedoutliers to show the outliers
+    this.jitter = 0.3;
+    this.pointpos = -1.8;
+    return this;
+  };
+
+  // edit all of those when you select the addText method
+  // you can use a </br> tag to go to the next line
+  addText = () => {
+    this.text = [];
+    this.textposition = "auto";
+    this.textinfo = "none";
+    this.insidetextorientation = this.type === "pie" ? "radial" : ""; // this is good for the pie charts
+    return this;
+  };
+
+  addHoverInfo = () => {
+    this.hoverinfo = "label+percent+name"; // can also be set to "none";
+    return this;
+  };
+
+  removeLegend = () => {
+    this.showLegend = false;
+    return this;
+  };
+
+  changeColorScale = () => {
+    this.marker = {
+      ...this.marker,
+      colorscale: "Jet", // Hot accompaign the hot with a reverse scale of true
+    };
+    return this;
+  };
+
+  addCustomColorScale = () => {
+    this.colorscale = [
+      [0, "rgb(166,206,227)"],
+      [0.25, "rgb(31,120,180)"],
+      [0.45, "rgb(178,223,138)"],
+      [0.65, "rgb(51,160,44)"],
+      [0.85, "rgb(251,154,153)"],
+      [1, "rgb(227,26,28)"],
+    ];
+    return this;
+  };
+
+  // this is for adding a normal contour when you have type contours
+  addContours = () => {
+    this.contours = {
+      coloring: "heatmap", // this can also be lines to have lines as a contour
+      showlabels: true,
+      labelfont: {
+        family: "Raleway",
+        size: 12,
+        color: "white",
+      },
+    };
+    this.ncontours = 20;
+    return this;
+  };
+
+  // this works when the plot is of the type
+  // histogram2dcontour
+  // hover it requires an x and y axis
+  styleContours = () => {
+    this.colorscale = "Hot";
+    this.ncontours = 20;
+    this.reversescale = true;
+    return this;
+  };
+}
+
+export class PlotLayout {
+  constructor(type) {
+    this.title = "plot title";
+    this.showlegend = true;
+    this.font = { color: "", size: 18 };
+    this.paper_bgcolor = "";
+    this.plot_bgcolor = "";
+    this.yaxis = {
+      title: "",
+      gridcolor: "",
+      titlefont: {
+        family: "Arial, sans-serif",
+        size: 18,
+        color: "",
+      },
+      showline: true,
+      zerolinecolor: "",
+      zerolinewidth: 2,
+    };
+
+    this.xaxis = {
+      title: this.xTicks,
+      showgrid: false,
+      autorange: true,
+      gridwidth: 1,
+      zeroline: false,
+      tickangle: 60,
+      range: [],
+      titlefont: {
+        family: "Arial, sans-serif",
+        size: 18,
+        color: this.fontColor,
+      },
+    };
+    this.legend = {
+      width: 500,
+      height: 500,
+      y: 0.5,
+      yref: "paper",
+      font: {
+        family: "Arial, sans-serif",
+        size: 20,
+        color: "grey",
+      },
+    };
+
+    // for bar charts
+    this.barmode = "stack"; // group, for histograms you can use barmode of stack or overlay
+    this.bargap = 0.15;
+    this.bargroupgap = 0.1;
+
+    //box plots
+    this.layout = {
+      boxmode: group,
+    };
   }
 }
